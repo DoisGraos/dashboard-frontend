f7e8d1c4382e688c1e70cae9ad1027119a2786e3
Gustavo Ribeiro <gustavo@doisgraos.com>
2023-02-18 10:10:56 -0300

    feat: custom page title

--- src/pages/[userId].tsx
+++ src/pages/[userId].tsx
@@ -2,11 +2,13 @@ import { UsersService } from "@/services";
 import styles from "@/styles/pages/[userId].module.css";
 import { LogoutOutlined } from "@ant-design/icons";
 import { Button } from "antd";
+import Head from "next/head";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
 const UserDashboard = () => {
   const router = useRouter();
+  const [loading, setLoading] = useState(true);
   const [user, setUser] = useState(null);
 
   const signOut = () => {
@@ -20,31 +22,49 @@ const UserDashboard = () => {
         const user = await UsersService.getUser(router.query.userId);
 
         setUser(user);
+        setLoading(false);
       }
     })();
   }, [router.query.userId]);
 
   return (
-    <main className={styles.main}>
-      {user && (
+    <>
+      {!loading && (
         <>
-          <header className={styles.header}>
-            <div>
-              <img src="/images/logo-square.png" className={styles.logo} /> Olá,{" "}
-              {user.name}!
-            </div>
-            <Button type="text" onClick={signOut}>
-              Sair <LogoutOutlined style={{ color: "red" }} />
-            </Button>
-          </header>
-          <object
-            data={`https://app.powerbi.com/view?r=${user.reportId}`}
-            className={styles.object}
-          />
-          <div className={styles.notToShare} />
+          <Head>
+            <title>{user.name} | Dois Grãos</title>
+            <meta
+              property="og:title"
+              content={`${user.name} | Dois Grãos`}
+              key="title"
+            />
+          </Head>
+          <main className={styles.main}>
+            {user && (
+              <>
+                <header className={styles.header}>
+                  <div>
+                    <img
+                      src="/images/logo-square.png"
+                      className={styles.logo}
+                    />{" "}
+                    Olá, {user.name}!
+                  </div>
+                  <Button type="text" onClick={signOut}>
+                    Sair <LogoutOutlined style={{ color: "red" }} />
+                  </Button>
+                </header>
+                <object
+                  data={`https://app.powerbi.com/view?r=${user.reportId}`}
+                  className={styles.object}
+                />
+                <div className={styles.notToShare} />
+              </>
+            )}
+          </main>
         </>
       )}
-    </main>
+    </>
   );
 };
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,7 +9,8 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>DoisGrãos</title>
+        <title>Dois Grãos</title>
+        <meta property="og:title" content="Dois Grãos" key="title" />
         <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
